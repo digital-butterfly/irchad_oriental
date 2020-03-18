ea820f35e3443f71c56c06c64f90a3376c0a2fd1
naourass derouichi <naourass@gmail.com>
2020-03-18 14:41:04 +0100

    new file:   public/css/front-office/bootstrap.min.css new file:   public/images/front-office/favicon.ico new file:   public/images/front-office/logo-icon-colorful.svg new file:   resources/views/front-office/layouts/minimal.blade.php modified:   resources/views/welcome.blade.php

---
+++ resources/views/front-office/layouts/minimal.blade.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>IRCHAD</title>
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <meta name="author" content="Digital Butterfly" />
+
+    <link rel="shortcut icon" href="images/front-office/favicon.ico">
+
+    <!-- Google fonts -->
+    {{-- <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,400,600,700" rel="stylesheet"> --}}
+
+    {{-- <!-- Pe-icon-7 icon -->
+    <link rel="stylesheet" type="text/css" href="css/front-office/pe-icon-7-stroke.css"> --}}
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="css/front-office/bootstrap.min.css" type="text/css">
+
+    <!--Material Icon -->
+    {{-- <link rel="stylesheet" type="text/css" href="css/front-office/materialdesignicons.min.css" /> --}}
+
+    <!-- Swiper CSS -->
+    {{-- <link rel="stylesheet" href="css/front-office/swiper.min.css"> --}}
+
+    <!-- magnific pop-up -->
+    {{-- <link rel="stylesheet" type="text/css" href="css/front-office/magnific-popup.css" /> --}}
+
+    <!--Slider-->
+    {{-- <link rel="stylesheet" href="css/front-office/owl.carousel.css" />
+    <link rel="stylesheet" href="css/front-office/owl.theme.css" />
+    <link rel="stylesheet" href="css/front-office/owl.transitions.css" /> --}}
+
+    <!-- MENU CUSTOM css -->
+    {{-- <link rel="stylesheet" type="text/css" href="css/front-office/menu.css"> --}}
+
+    <!-- Custom  Css -->
+    {{-- <link rel="stylesheet" type="text/css" href="css/front-office/style.css" /> --}}
+
+</head>
+
+<body>
+
+    @yield('content')
+
+    {{-- <!-- JAVASCRIPTS -->
+    <script src="js/front-office/jquery.min.js"></script>
+    <script src="js/front-office/popper.min.js"></script>
+    <script src="js/front-office/bootstrap.min.js"></script>
+    <script src="js/front-office/jquery.easing.min.js"></script>
+    <script src="js/front-office/scrollspy.min.js"></script>
+    <!-- owl-carousel -->
+    <script src="js/front-office/owl.carousel.min.js"></script>
+    <!-- Swiper JS -->
+    <script src="js/front-office/swiper.min.js"></script>
+    <!-- Magnific Popup -->
+    <script src="js/front-office/jquery.magnific-popup.min.js"></script>
+    <script src="js/front-office/app.js"></script> --}}
+
+    @yield('custom-js')
+
+</body>
+
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+{{-- <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
@@ -98,3 +98,166 @@
         </div>
     </body>
 </html>
+ --}}
+
+@extends('front-office.layouts.minimal')
+
+@section('content')
+
+<style>
+html,
+body {
+  height: 100%;
+}
+
+body {
+    display: -ms-flexbox;
+    display: flex;
+    -ms-flex-align: center;
+    align-items: center;
+    padding-top: 40px;
+    padding-bottom: 40px;
+    background-color: #f5f5f5;
+}
+
+.form-signin {
+    width: 100%;
+    max-width: 330px;
+    padding: 15px;
+    margin: auto;
+}
+.form-signin .checkbox {
+    font-weight: 400;
+}
+.form-signin .form-control {
+    position: relative;
+    box-sizing: border-box;
+    height: auto;
+    padding: 10px;
+    font-size: 16px;
+}
+.form-signin .form-control:focus {
+    z-index: 2;
+}
+.form-signin input[type="email"] {
+    margin-bottom: -1px;
+    border-bottom-right-radius: 0;
+    border-bottom-left-radius: 0;
+}
+.form-signin input[type="password"] {
+    margin-bottom: 10px;
+    border-top-left-radius: 0;
+    border-top-right-radius: 0;
+}
+.close {
+  position: absolute;
+  right: 32px;
+  top: 32px;
+  width: 32px;
+  height: 32px;
+  opacity: 0.3;
+}
+.close:hover {
+  opacity: 1;
+}
+.close:before, .close:after {
+  position: absolute;
+  left: 15px;
+  content: ' ';
+  height: 33px;
+  width: 2px;
+  background-color: #333;
+}
+.close:before {
+  transform: rotate(45deg);
+}
+.close:after {
+  transform: rotate(-45deg);
+}
+
+/* Temporary loading animation */
+@keyframes blink {
+    /**
+     * At the start of the animation the dot
+     * has an opacity of .2
+     */
+    0% {
+      opacity: .2;
+    }
+    /**
+     * At 20% the dot is fully visible and
+     * then fades out slowly
+     */
+    20% {
+      opacity: 1;
+    }
+    /**
+     * Until it reaches an opacity of .2 and
+     * the animation can start again
+     */
+    100% {
+      opacity: .2;
+    }
+}
+
+.saving span {
+    /**
+     * Use the blink animation, which is defined above
+     */
+    animation-name: blink;
+    /**
+     * The animation should take 1.4 seconds
+     */
+    animation-duration: 1.4s;
+    /**
+     * It will repeat itself forever
+     */
+    animation-iteration-count: infinite;
+    /**
+     * This makes sure that the starting style (opacity: .2)
+     * of the animation is applied before the animation starts.
+     * Otherwise we would see a short flash or would have
+     * to set the default styling of the dots to the same
+     * as the animation. Same applies for the ending styles.
+     */
+    animation-fill-mode: both;
+}
+
+.saving span:nth-child(2) {
+    /**
+     * Starts the animation of the third dot
+     * with a delay of .2s, otherwise all dots
+     * would animate at the same time
+     */
+    animation-delay: .2s;
+}
+
+.saving span:nth-child(3) {
+    /**
+     * Starts the animation of the third dot
+     * with a delay of .4s, otherwise all dots
+     * would animate at the same time
+     */
+    animation-delay: .4s;
+}
+</style>
+
+{{-- <a href="/" class="close"></a> --}}
+<form class="form-signin text-center">
+    <img class="mb-2" src="images/front-office/logo-icon-colorful.svg" alt="" width="144" height="144">
+    <h1 class="h3 mb-3 font-weight-normal">Connectez vous</h1>
+    <label for="inputEmail" class="sr-only">Adresse email</label>
+    <input type="email" id="inputEmail" class="form-control" placeholder="Adresse email" required="" autofocus="">
+    <label for="inputPassword" class="sr-only">Mot de passe</label>
+    <input type="password" id="inputPassword" class="form-control" placeholder="Mot de passe" required="">
+    <div class="checkbox mb-3">
+        <label>
+            <input type="checkbox" value="remember-me"> Rester connecté
+        </label>
+    </div>
+    <button class="btn btn-lg btn-primary btn-block" type="submit">Se connecter</button>
+    {{-- <p class="mt-5 mb-3 text-muted">© IRCHAD 2020</p> --}}
+    <p class="mt-5 mb-3 text-muted saving">Système en cours de préparation, veuillez patienter pendant que nous finalisons la configuration <span>.</span><span>.</span><span>.</span> </p>
+</form>
+
+@endsection
