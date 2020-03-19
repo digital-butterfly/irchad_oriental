da94489784e51e80d45ccc6ab96b1a8b54512ce7
naourass derouichi <naourass@gmail.com>
2020-03-20 00:56:55 +0100

    Commit 	modified:   app/Http/Controllers/Auth/RegisterController.php 	modified:   resources/views/auth/register.blade.php

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -56,7 +56,7 @@ class RegisterController extends Controller
         return Validator::make($data, [
             'first_name' => ['required', 'string', 'max:255'],
             'last_name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:members'],
             'password' => ['required', 'string', 'min:4', 'confirmed'],
         ]);
     }
@@ -65,7 +65,7 @@ class RegisterController extends Controller
      * Create a new user instance after a valid registration.
      *
      * @param  array  $data
-     * @return \App\User
+     * @return \App\Member
      */
     protected function create(array $data)
     {
