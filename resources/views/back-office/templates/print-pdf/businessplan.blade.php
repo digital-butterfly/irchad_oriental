4a5b7c43f4f1f7a607e83dcdbc7afcc0604a013b
wafaeaissaoui <wafaeissa17@gmail.com>
2022-02-10 13:33:45 +0100

    fix(businessplan):fixed some problems

--- resources/views/back-office/templates/print-pdf/businessplan.blade.php
+++ resources/views/back-office/templates/print-pdf/businessplan.blade.php
@@ -6992,20 +6992,7 @@ optionTwo && chartDomOne.setOption(optionTwo);
 </script>
 
 
-/*    function download() {
-      var element = document.getElementById("test");
-      var opt = {
-        margin: [10, 0, 10, 0],
-        filename: `document.pdf`,
-        image: { type: "jpeg", quality: 0.98 },
-        html2canvas: { scale: 2, useCORS: true },
-        jsPDF: { unit: "mm", format: "letter", orientation: "portrait" },
-      };
 
-      html2pdf().from(element).save("myfile.pdf");
-    }
-
-*/
 
 
 
